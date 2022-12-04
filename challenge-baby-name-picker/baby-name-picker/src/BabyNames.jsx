aef9a25d25aff234b2f199758f550c6489f0ceb9
Alex Phillip <alexjoelphillip@gmail.com>
2022-12-04 21:21:49 +0000

    sorted baby names alphabetically

--- challenge-baby-name-picker/baby-name-picker/src/BabyNames.jsx
+++ challenge-baby-name-picker/baby-name-picker/src/BabyNames.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import BabyNamesData from "./babyNamesData.json";
 
+function sortAlphabetically(a, b) {
+    if (a.name < b.name) {
+        return -1;
+    } else if (a.name > b.name) {
+        return 1;
+    } else {
+        return 0;
+    };
+};
+
 const BabyNames = () => {
     return (
       <div>
         <ul>
-          {BabyNamesData.map(({ name }) => (
+          {BabyNamesData.sort(sortAlphabetically).map(({ name }) => (
             <li key={name}>{name}</li>
           ))}
         </ul>
